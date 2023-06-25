455177c37e097b325330367fa142e262231984ed
Agis <agismaciel@hotmail.com>
2023-06-25 15:32:45 -0300

    "Iclusão da Logo e alterações em cores - PII"

--- js/style.js
+++ js/style.js
@@ -1,4 +1,4 @@
-git commit -m document.getElementById("loginForm").addEventListener("submit", function(event) {
+document.getElementById("loginForm").addEventListener("submit", function(event) {
     event.preventDefault();
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
@@ -8,4 +8,22 @@ git commit -m document.getElementById("loginForm").addEventListener("submit", fu
     console.log("Username: " + username);
     console.log("Password: " + password);
   });
-  
+  // Localize a div com a classe "LogoIF"
+  var logoDiv = document.querySelector('.LogoIF');
+
+  // Crie um elemento de imagem
+  var logoImg = document.createElement('img');
+  logoImg.src = 'caminho/para/sua/imagem.png';
+  logoImg.alt = 'Logo';
+
+  // Defina o estilo para centralizar a imagem
+  logoImg.style.display = 'block';
+  logoImg.style.margin = 'auto';
+  logoImg.style.position = 'absolute';
+  logoImg.style.top = '0';
+  logoImg.style.bottom = '0';
+  logoImg.style.left = '0';
+  logoImg.style.right = '0';
+
+  // Insira a imagem dentro da div "LogoIF"
+  logoDiv.appendChild(logoImg);
